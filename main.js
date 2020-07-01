f1b46bd0e58e20d6046e33a306c9a245d993d0e6
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-07-01 15:27:33 -0400

    almost

--- main.js
+++ main.js
@@ -1,6 +1,68 @@
 /******************
  * YOUR CODE HERE *
 ******************/
+const doubleAll = function(arr){
+  let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(arr[i] * 2);
+    }
+    return newArr;
+}
+
+const yelledGreetings = function(arr){
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(arr[i] + '!')
+  }
+  return newArr;
+}
+
+const absoluteValues = function(arr){
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+      if(arr > 0 ? arr * -1 : arr ){
+        newArr.push(arr[i])
+      }
+        }
+    return newArr;
+}
+
+const upperCaseFirstLetters = function(arr){
+  let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+      let lowerCase = arr[i].slice(1, arr[i].length).toLowerCase();
+      newArr.push(arr[i][0].toUpperCase() + lowerCase);
+    }
+    return newArr
+}
+
+const changeToInitials = function(arr){
+  let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+      newArr.push(arr[i][0] + arr[i][arr[i].indexOf(' ') + 1])
+    }
+    return newArr
+}
+
+const doubleOdd = function(arr){
+  let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+      if (arr[i] % 2 === 1 || arr[i] % 2 === -1){
+        newArr.push(arr[i] * 2);
+      } else {
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
+}
+
+const add1ToLeft = function(arr){
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    
+  }
+  return newArr;
+}
 
 
 
